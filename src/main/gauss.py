821d62c9be586263fbbc0dd699057630d1af5d76
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-10-01 21:14:48 +0900

    関数mixed_gaussについて、関数gaussと関数mixed_gaussの出力(np.ndarray型)が一致. - src/main/gauss.py -- 関数mixed_gauss --- 単峰ガウス分布をnp.ndarray型配列として引数にとり、np.arrayコンストラクタに入れて返す。 - src/test/test_gauss.py -- 関数test_gauss_assertequal_gauss_mixed_gauss --- ↑上記を確認.

--- src/main/gauss.py
+++ src/main/gauss.py
@@ -3,12 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-sample_size = 100
+sample_size = 100   # サンプルサイズ
 
 def gauss(x, mu, sigma):
+    '''
+    (単峰)ガウス分布を、np.ndarray型で返す.
+    '''
     gauss_y = np.exp(-(x-mu)**2 / 2*sigma**2)     \
                     / np.sqrt(2 * np.pi * sigma**2)     # 上行:密度関数の本体, 下行:規格化定数
     return gauss_y
+
+def mixed_gauss(x, input_gauss):
+    '''
+    混合ガウス分布を、np.ndarray型で返す.
+    '''
+    mixed_gauss_y = np.array(input_gauss)
+    return mixed_gauss_y
 
 def main():
     sigma = 1   # 標準偏差
@@ -16,6 +26,7 @@ def main():
     #x = [random.gauss(mu=0, sigma=1) for _ in range(sample_size)]
     x = np.linspace(-3, 3, sample_size)
     gauss_y = gauss(x, mu=0, sigma=1)
+    mixed_gauss_y = mixed_gauss(x, gauss_y)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(x, gauss_y)
--- src/test/test_gauss.py
+++ src/test/test_gauss.py
@@ -22,12 +22,22 @@ class TestGauss(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_gauss_gauss_return_type(self):
+    def test_gauss_mixedgauss_parameter(self):
         '''
-        gaussモジュールについて、関数gaussの返り値の型を確認.
+        gaussモジュールについて、関数mixed_gaussの引数をx (np.ndarray) とinput_gauss (np.ndarray) の2つに変更し、
+        正しく動作 (np.ndarray型を返すかどうか) を確認.
+        '''
+        mixed_gauss_y = gauss.mixed_gauss(self.x, gauss.gauss(self.x, 0, 1))
+        self.assertIsInstance(mixed_gauss_y, np.ndarray)  # numpy.ndarray配列型かアサーション
+
+    def test_gauss_assertequal_gauss_mixed_gauss(self):
+        '''
+        関数mixed_gaussについて、関数gaussと関数mixed_gaussの出力 (np.ndarray型) が一致するか確認.
+        ただし、関数mixed_gaussが受け取る引数は、xと1つの標準正規分布のみ。
         '''
         gauss_y = gauss.gauss(self.x, 0, 1)
-        self.assertIsInstance(gauss_y, np.ndarray)  # numpy.ndarray配列型かアサーション
+        mixed_gauss_y = gauss.mixed_gauss(self.x,gauss_y)
+        np.testing.assert_array_equal(gauss_y, mixed_gauss_y)
 
 
 if __name__ == '__main__':
