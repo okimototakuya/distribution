7ea3956c09b2016cb5175eab46d6f31dfed14fbe
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-10-04 23:09:13 +0900

    メトロポリス法を関数化(metropolis)

--- src/main/sampling.py
+++ src/main/sampling.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 sys.path.append('.')
 import gauss
 
+sample_list = []
+sample_list.append(10) # 初期値を適当に10と定めた
 
-def main():
+def metropolis(p):
     '''
     メトロポリス法を実装.
     メトロポリス・ヘイスティングス法の簡易版.
     代理分布: Normal(sample_list[i], 1)
+
+    Parameters
+    ------
+    p : function
+        所望の分布
     '''
-    mu = 0
-    sigma = 1
-    p = lambda theta: gauss.gauss(theta, mu=mu, sigma=sigma)  # 所望の分布
-
-    sample_list = []
-    sample_list.append(10) # 初期値を適当に10と定めた
-
     for i in range(100000):
         theta = np.random.normal(sample_list[i], 1) # 提案値
         a = min(1, p(theta) / p(sample_list[i]))
@@ -30,6 +30,13 @@ def main():
         else: # 拒否
             sample_list.append(sample_list[i])  # 1つ前の標本を保持
 
+def main():
+    # 所望の分布
+    mu = 0
+    sigma = 1
+    p = lambda theta: gauss.gauss(theta, mu=mu, sigma=sigma)    # theta : intまたはnp.ndarray
+    metropolis(p)   # メトロポリス法
+    # 分布のプロット
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax = plt.hist(sample_list, bins=100)
