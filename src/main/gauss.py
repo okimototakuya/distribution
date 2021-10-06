6882c4ab380ea9976d822a78ed31a970de4c98fb
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-10-06 23:39:03 +0900

    パラメータを1次元にした場合で、標準正規分布以外でも、gauss.gaussとgauss.multidim_gaussの返り値がほぼ一致した. - gauss.gaussには標準偏差を与え、gauss.multidim_gaussには分散を与えること. - おそらく数値誤差のため、完全には値が一致しない.

--- src/main/gauss.py
+++ src/main/gauss.py
@@ -11,17 +11,32 @@ def gauss(x, mu, sigma):
     '''
     (単峰)ガウス分布を、np.ndarray型で返す.
 
+    Parameters
+    -----
+    x: 確率変数値 (int, numpy.ndarray, numpy.matrix)
+    mu: 期待値スカラー (int)
+    sigma: 標準偏差スカラー (int)   注. sigmaは分散でなく、標準偏差であることに注意すること. 
+
     Note
     -----
     - numpy=1.20.1/numpy-base=1.20.1の環境において、multidim_gaussのパラメータを1次元にした場合、返り値が一致した.
-    - ↑ただし、標準正規分布の場合について。
+    - 2021.10.6 昼頃: ↑ただし、標準正規分布の場合について。
+    - 2021.10.6 夜: ↑標準正規分布以外で、値がほぼ一致することを確認した。おそらく数値誤差のよる差分。
     '''
-    gauss_y = np.exp(-(x-mu)**2 / 2*sigma**2)     \
-                    / np.sqrt(2 * np.pi * sigma**2)     # 上行:密度関数の本体, 下行:規格化定数
+    gauss_y = np.exp(-((x-mu)**2) / (2*(sigma**2)))     \
+                    / (np.sqrt(2*np.pi) * sigma)     # 上行:密度関数の本体, 下行:規格化定数
     return gauss_y
 
 def multidim_gauss(x, mu, sigma):
     '''
+    多変量ガウス分布を、np.ndarray型で返す.
+
+    Parameters
+    -----
+    x: 確率変数値 (int, numpy.ndarray, numpy.matrix)
+    mu: 期待値ベクトル (numpy.matrix配列の転置)
+    sigma: 共分散分散行列 (numpy.matrix行列)            # 1変量分布の場合、標準偏差でなく分散を与えること.
+
     Notes
     -----
     - [参考]: https://qiita.com/g-k/items/698c7f9e4a213d73197b
@@ -44,25 +59,27 @@ def mixed_gauss(x, input_gauss):
 
 def main():
     '''
+    分布をプロットする.
     '''
     # 1. 座標軸の設定
-    #x = np.linspace(-3, 3, sample_size)        # 単変量
-    x = y = np.linspace(-3, 3, sample_size)     # ２変量
-    X, Y = np.meshgrid(x, y)
-    z = np.c_[X.ravel(),Y.ravel()]
+    x = np.linspace(-3, 3, sample_size)        # 単変量
+    #x = y = np.linspace(-3, 3, sample_size)     # ２変量
+    #X, Y = np.meshgrid(x, y)
+    #z = np.c_[X.ravel(),Y.ravel()]
     # 2. ガウス分布の密度関数
-    #gauss_y = gauss(x, mu=0, sigma=1)           # (単変量)ガウス分布
+    gauss_y = gauss(x, mu=0, sigma=np.sqrt(5))           # (単変量)ガウス分布
     #Z = multidim_gauss(z, mu=np.array([0, 0]), sigma=np.array([[1, 0], [0, 1]]))  # 多変量ガウス分布
-    Z = multidim_gauss(z.T, mu=np.matrix([0, 0]).T, sigma=np.matrix([[1, 0], [0, 1]]))  # 多変量ガウス分布
+    #Z = multidim_gauss(z.T, mu=np.matrix([0, 0]).T, sigma=np.matrix([[1, 0], [0, 1]]))  # 多変量ガウス分布
+    #gauss_y = multidim_gauss(x.T, mu=np.matrix([0]).T, sigma=np.matrix([5]))  # 多変量ガウス分布
     #gauss_y = mixed_gauss(x, gauss_y)     # (単変量)混合ガウス分布
     # 3. ガウス分布のプロット
-    shape = X.shape
-    Z = Z.reshape(shape)
+    #shape = X.shape
+    #Z = Z.reshape(shape)
     fig = plt.figure()
-    #ax = fig.add_subplot(111)
-    ax = fig.add_subplot(111, projection='3d')
-    #ax.plot(x, gauss_y)
-    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm)
+    ax = fig.add_subplot(111)
+    #ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, gauss_y)
+    #ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm)
     plt.show()
 
 
--- src/test/test_gauss.py
+++ src/test/test_gauss.py
@@ -22,7 +22,7 @@ class TestGauss(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def _test_gauss_mixedgauss_parameter(self):
+    def test_gauss_mixedgauss_parameter(self):
         '''
         gaussモジュールについて、関数mixed_gaussの引数をx (np.ndarray) とinput_gauss (np.ndarray) の2つに変更し、
         正しく動作 (np.ndarray型を返すかどうか) を確認.
@@ -30,7 +30,7 @@ class TestGauss(unittest.TestCase):
         mixed_gauss_y = gauss.mixed_gauss(self.x, gauss.gauss(self.x, 0, 1))
         self.assertIsInstance(mixed_gauss_y, np.ndarray)  # numpy.ndarray配列型かアサーション
 
-    def test_gauss_assertequal_gauss_mixed_gauss(self):
+    def test_mixed_gauss_given_1_standard_gauss(self):
         '''
         関数mixed_gaussについて、関数gaussと関数mixed_gaussの出力 (np.ndarray型) が一致するか確認.
         ただし、関数mixed_gaussが受け取る引数は、xと1つの標準正規分布のみ。
@@ -42,36 +42,36 @@ class TestGauss(unittest.TestCase):
     def test_multidim_gauss_given_1_dimensional_parameter(self):
         '''
         gaussモジュールについて、関数multidim_gaussに1次元の引数を与えた場合に、関数gaussと関数multidim_gaussの返す値が一致するかテスト.
-
-        Notes
-        -----
-        予想外に見事に一致した！
         '''
-        y = gauss.gauss(self.x, 0, 1)
-        z = gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([1]))
-        ## 関数gauss.gauss
+        ## 配列同士の差
+        #y = gauss.gauss(self.x, 0, np.sqrt(2))
+        #z = gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([2]))
+        #print(y-z)
+        ## 関数gauss.gaussについて
         #print('gauss.gaussが返す値:{ret_val}'.format(ret_val=y))
         #print('gauss.gaussが返す型:{ret_type}'.format(ret_type=type(y)))
         #print('gauss.gaussが返す値の大きさ:{ret_val_size}'.format(ret_val_size=len(y)))
-        ## 関数gauss.multidim_gauss
+        ## 関数gauss.multidim_gaussについて
         #print('gauss.multidim_gaussが返す値:{ret_val}'.format(ret_val=z))
         #print('gauss.multidim_gaussが返す型:{ret_type}'.format(ret_type=type(z)))
         #print('gauss.multidim_gaussが返す値の大きさ:{ret_val_size}'.format(ret_val_size=len(z)))
-        np.testing.assert_array_equal(gauss.gauss(self.x, 0, 1), gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([1])))
+        ## アサーション
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 0, 1), gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([1])))                   #1. 単変量標準正規分布同士 (通る)
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 0, 1), gauss.multidim_gauss(self.x.T, mu=np.matrix([0, 0]).T, sigma=np.matrix([[1, 0], [0, 1]])))   #2. 単変量 " と2変量 " (通らない)
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 34, 1), gauss.multidim_gauss(self.x.T, mu=np.matrix([34]).T, sigma=np.matrix([1])))                 #3. 期待値mu not eq 0 (通る)
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 0, np.sqrt(2)), gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([2])))          #4. 分散sigma not eq 1 (通らない)
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 45, 2), gauss.multidim_gauss(self.x.T, mu=np.matrix([45]).T, sigma=np.matrix([2])))                 #5. 3かつ4 (通らない)
+        #np.testing.assert_array_equal(gauss.gauss(self.x, 3, np.sqrt(2)), gauss.multidim_gauss(self.x.T, mu=np.matrix([3]).T, sigma=np.matrix([2])))          #6. gauss.gaussの方は標準偏差を与える (通らないが、値はほぼ一致. おそらく数値誤差の分)
+        np.testing.assert_array_almost_equal(gauss.gauss(self.x, 0, np.sqrt(2)), gauss.multidim_gauss(self.x.T, mu=np.matrix([0]).T, sigma=np.matrix([2])))    #7. assertAlmostEqualでテスト(通る)
+        ## assertAlmostEqualについて、学習用テスト
+        ### スカラー
+        #self.assertAlmostEqual(1, 0.99999996)   # assertAlmostEqualの仕様: 有効数字が小数点以下8桁 かつ 8桁目が6以上で通る.
+        ### シーケンス
+        #self.assertAlmostEqual([1, 2, 3], [1, 2, 3])                                                                               # 通る
+        #self.assertAlmostEqual([1, 2, 3], [1, 2, 9])                                                                               # 通らない (標準リストだと、-演算がサポートされていないため.)
+        #np.testing.assert_array_almost_equal(np.array([1, 2, 3]), np.array([1, 2, 3]))                                             # 通る
+        #np.testing.assert_array_almost_equal(np.array([i for i in range(100)]), np.array([i for i in range(100)][:99]+[99]))       # 通る (要素の非合致率1% かつ その値の差が1. しきい値は不明.)
 
-    def test_multidim_gauss_ret_val(self):
-        '''
-        関数multidim_gaussについて、返り値の詳細をテスト.
-
-        Notes
-        -----
-        帰り値の値や型、大きさなど.
-        '''
-        z = gauss.multidim_gauss(self.x.T, mu=np.matrix([0, 0]).T, sigma=np.matrix([[1, 0], [0, 1]]))
-        print('gauss.multidim_gaussが返す値:{ret_val}'.format(ret_val=z))
-        print('gauss.multidim_gaussが返す型:{ret_type}'.format(ret_type=type(z)))
-        print('gauss.multidim_gaussが返す値の大きさ:{ret_val_size}'.format(ret_val_size=len(z)))
-        self.assertIsInstance(z, np.ndarray)    # 通る
 
 if __name__ == '__main__':
     unittest.main()
