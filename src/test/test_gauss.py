e737bc0d2658a94aa23659a5ee6f89b43b52a0d1
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air.local>
2021-10-08 13:33:03 +0900

    関数multidim_gaussを、関数mixed_gaussに渡して正常に動作するかテストした. - multidim_gaussの返り値が、単変量/多変量分布を表すnp.ndaaray型配列どちらの場合もテストした.

--- src/test/test_gauss.py
+++ src/test/test_gauss.py
@@ -48,12 +48,30 @@ class TestGauss(unittest.TestCase):
         mixed_gauss_y = gauss.mixed_gauss(self.x, gauss_y, gauss_y)
         np.testing.assert_array_equal(gauss_y*2, mixed_gauss_y)   # 通る.
 
-    def test_mixed_gauss_given_2_standard_gauss_who_has_mixture_ratio(self):
+    def _test_mixed_gauss_given_2_standard_gauss_who_has_mixture_ratio(self):
         '''
         関数mixed_gaussについて、関数gaussの出力と関数mixed_gaussの出力 (np.ndarray型) が一致するか確認.
         ただし、関数mixed_gaussが受け取る引数は、xと2つの混合率を持つ標準正規分布のみ。
         '''
         gauss_y = gauss.gauss(self.x, 0, 1)
+        mixed_gauss_y = gauss.mixed_gauss(self.x, (gauss_y, 1/2), (gauss_y, 1/2))
+        np.testing.assert_array_equal(gauss_y, mixed_gauss_y)   # 通る.
+
+    def _test_mixed_gauss_given_2_standard_multidim_gauss_who_has_mixture_ratio(self):
+        '''
+        関数mixed_gaussについて、関数multidim_gaussの出力と関数mixed_gaussの出力 (np.ndarray型) が一致するか確認.
+        ただし、関数mixed_gaussが受け取る引数は、xと2つの混合率を持つ単変量標準正規分布のみ。
+        '''
+        gauss_y = gauss.multidim_gauss(self.x.T, np.matrix([0]).T, np.matrix([1]))
+        mixed_gauss_y = gauss.mixed_gauss(self.x, (gauss_y, 1/2), (gauss_y, 1/2))
+        np.testing.assert_array_equal(gauss_y, mixed_gauss_y)   # 通る.
+
+    def test_mixed_gauss_given_2_standard_multidim_gauss_who_has_mixture_ratio(self):
+        '''
+        関数mixed_gaussについて、関数multidim_gaussの出力と関数mixed_gaussの出力 (np.ndarray型) が一致するか確認.
+        ただし、関数mixed_gaussが受け取る引数は、xと2つの混合率を持つ多変量標準正規分布のみ。
+        '''
+        gauss_y = gauss.multidim_gauss(self.x.T, np.matrix([0,0]).T, np.matrix([[1,0],[0,1]]))
         mixed_gauss_y = gauss.mixed_gauss(self.x, (gauss_y, 1/2), (gauss_y, 1/2))
         np.testing.assert_array_equal(gauss_y, mixed_gauss_y)   # 通る.
 
