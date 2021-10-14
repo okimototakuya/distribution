cf33b3af0d26e009fcc28498dbcc3bc8371bd627
沖本卓也 <okimototakuya@okimototakuyanoMacBook-Air-3.local>
2021-10-14 22:32:09 +0900

    config.pyのプロトタイプを作成

---
+++ src/main/config.py
@@ -0,0 +1,20 @@
+class Configuration():
+    '''
+    '''
+    @parameters
+    '''
+    '''
+    def parameters(self):
+        return __hoge-hoge
+
+    @parameters.setter
+    '''
+    '''
+    def parameters(self, input_hoge-hoge):
+        self.__hoge-hoge = input_hoge-hoge
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
